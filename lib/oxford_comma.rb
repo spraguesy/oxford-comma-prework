bdf64de684853e85ac78f78cce59c77e9859d15b
Andrew Sprague <andrewsprague@outlook.com>
2018-08-13 14:06:01 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,7 +4,7 @@ def oxford_comma(array)
   elsif array.length == 2
     array.join(" and ")
   elsif array.length == 3
-    last_word = "and" << array.pop()
+    last_word = "and " << array.pop()
     array.push(last_word)
     array.join(", ")
   else
