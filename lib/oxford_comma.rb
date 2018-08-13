9ab7bbdbaaeaefaed22d5a02966254b339fb960d
Andrew Sprague <andrewsprague@outlook.com>
2018-08-13 13:50:12 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,6 +2,7 @@ def oxford_comma(array)
   if array.length == 2
     array.join(" and ")
   else
+    array[array.length -1] = "and" << array[array.length -1]
     array.join(", ")
-    a
+  end
 end
