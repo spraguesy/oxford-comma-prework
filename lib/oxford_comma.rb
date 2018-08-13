885118bf760793521e2d47ea562191848032cc7a
Andrew Sprague <andrewsprague@outlook.com>
2018-08-13 14:06:11 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,15 @@
 def oxford_comma(array)
-
+  if array.length == 1
+    array[array.length-1]
+  elsif array.length == 2
+    array.join(" and ")
+  elsif array.length == 3
+    last_word = "and " << array.pop()
+    array.push(last_word)
+    array.join(", ")
+  else
+    last_word = "and " << array.pop()
+    array.push(last_word)
+    array.join(", ")
+  end
 end
