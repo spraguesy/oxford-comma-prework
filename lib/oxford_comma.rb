7b7416fa4f7968e30083484af19879da0f10d131
Andrew Sprague <andrewsprague@outlook.com>
2018-08-13 14:05:38 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,14 +4,12 @@ def oxford_comma(array)
   elsif array.length == 2
     array.join(" and ")
   elsif array.length == 3
-    last_word = "and" << array[array.length -1]
-    puts last_word
-    array[array.length -1] = last_word
+    last_word = "and" << array.pop()
+    array.push(last_word)
     array.join(", ")
   else
-    last_word = "and" << array[array.length -1]
-    puts last_word
-    array[array.length -1] = last_word
+    last_word = "and" << array.pop()
+    array.push(last_word)
     array.join(", ")
   end
 end
